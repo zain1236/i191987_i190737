f39462ad9f4477e2b72826008cce3a4cca78e087
Zain <saimzain1236@gmail.com>
2022-10-22 17:37:32 -0700

    Initial commit

---
+++ app/src/main/java/com/ass2/i191987_i190737/MainActivity.java
@@ -0,0 +1,14 @@
+package com.ass2.i191987_i190737;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
